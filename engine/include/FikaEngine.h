72f9b31aec857877cb5597fb73454d54efad9c5a
jw-xplore <jwernerltug@gmail.com>
2026-05-03 11:08:58 +0200

    Separated projects folder, Single engine include header

---
+++ engine/include/FikaEngine.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// Core
+#include "core/application.h"
+
+// Render
+#include "renderer/renderer.h"
+#include "renderer/resources/gResourceManager.h"
+#include "renderer/resources/meshInstance.h"
+#include "renderer/resources/meshBuilder.h"
+
+// Plaftorm
+#include "platform/window.h"
+#include "platform/input.h"
---
+++ projects/testgame/src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <FikaEngine.h>
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+int main()
+{
+    Application app;
+    app.run();
+}
